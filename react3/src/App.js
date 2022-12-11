021cb410eb9d92bfe8853bc86b2820038722b176
yeebryan <bryanwebiz@gmail.com>
2022-12-11 16:24:29 +0800

    update all for Mall2

--- react3/src/App.js
+++ react3/src/App.js
@@ -1,4 +1,12 @@
+// USE Bootstrap
+// TO see where ur bootstrap is
+// CAN'T FIND THEN - npm install bootstrap
+// go to NODE_Module > bootstrap > dist > css > bootstrap.min.css
+// * NOTE WHENEVER We do a INSTALL, it will go under NODE_MODULE
+// IMPORT bootstrap
 
+import Mall2 from "./Mall2"
+import "bootstrap/dist/css/bootstrap.min.css"
 import MallDirectory from "./MallDirectory";
 
 function App() {
