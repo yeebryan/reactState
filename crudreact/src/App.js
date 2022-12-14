45ae0ecf216c289c7bb70eb9da785799d6f47a9c
yeebryan <bryanwebiz@gmail.com>
2022-12-14 22:58:47 +0800

    simple LIST CRUD

---
+++ crudreact/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TaskList from './TaskList'
+
+function App(){
+  return(
+    <div>
+      <TaskList/>
+    </div>
+  )
+}
+
+export default App;
---
+++ crudreact/src/TaskList.js
@@ -0,0 +1,32 @@
+// React Lab 9 - CRUD
+
+// Build toDo List 
+// Ask user to 
+// (1) add LIST of TASKS (2) MARK THEM AS DONE (3) DELETE/CHANGE TASK'S DESCRIPTION
+
+import React from 'react'
+
+export default class TaskList extends React.Component{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    render(){
+        return <React.Fragment>
+            <h1>Todo List</h1>
+        </React.Fragment>
+    }
+}
